ea0e3fec369ef301ce2c3d1e2f6b4458a71a0bfb
jaychang99 <jaychang99@gmail.com>
2023-09-27 23:05:53 +0900

    fix(CORS): localhost:3000 cors 오픈

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(8000);
 }
