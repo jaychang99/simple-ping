afd079a913631db888e63bd76dec7272c640a06e
jaychang99 <jaychang99@gmail.com>
2023-09-27 17:29:36 +0900

    fix(App): 반복문 대기 안 걸리는 문제 해결

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,7 +11,6 @@ const HEADERS = {
     'Content-Type': 'application/json',
   },
 };
-const DEFAULT_CHECK_SITE = 'https://www.naver.com';
 
 @Injectable()
 export class AppService {
@@ -56,49 +55,7 @@ export class AppService {
           HEADERS,
         );
       }
-
-      return;
-
-      // const checkedSite = site ?? DEFAULT_CHECK_SITE;
-      // console.log('CRON JOB STARTED');
-      // try {
-      //   await axios.get(checkedSite);
-      //   try {
-      //     await axios.post(
-      //       this.config.get('SLACK_WEBHOOK_URL'),
-      //       generateSlackReportingSuccess(checkedSite),
-      //       HEADERS,
-      //     );
-      //   } catch (err) {
-      //     console.log(err);
-      //   }
-
-      //   return {
-      //     status: 'UP',
-      //     site: site,
-      //     checkedAt: new Date().toISOString(),
-      //   };
-      // } catch (err) {
-      //   console.log(err);
-      //   const error = err as AxiosError;
-
-      //   const errorCode = error.code;
-
-      //   try {
-      //     await axios.post(
-      //       this.config.get('SLACK_WEBHOOK_URL'),
-      //       generateSlackReporting(checkedSite, errorCode),
-      //       HEADERS,
-      //     );
-      //   } catch (err) {
-      //     console.log(err);
-      //   }
-
-      //   return {
-      //     status: 'DOWN',
-      //     site: checkedSite,
-      //     checkedAt: new Date().toISOString(),
-      //   };
     }
+    return;
   }
 }
