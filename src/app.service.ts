61e60830586527a7f4215449b6f41d3df8853325
jaychang99 <jaychang99@gmail.com>
2023-09-27 14:40:00 +0900

    feat: 슬랙 웹훅 테스트

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import axios, { AxiosError } from 'axios';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async checkSite(site: string) {
+    if (!site) throw new HttpException('Site is required!', 400);
+
+    try {
+      await axios.get(site);
+
+      return {
+        status: 'UP',
+        site: site,
+        checkedAt: new Date().toISOString(),
+      };
+    } catch (err) {
+      console.log(err);
+      const error = err as AxiosError;
+
+      const errorCode = error.code;
+
+      try {
+        await axios.post(
+          process.env.SLACK_WEBHOOK_URL,
+          {
+            text: `🚫 ${site} 서비스가 다운되었습니다. 🚫`,
+            blocks: [
+              {
+                type: 'section',
+                text: {
+                  type: 'mrkdwn',
+                  text: `🚫 ${site} 서비스가 다운되었습니다. 🚫 ERROR CODE: ${errorCode}`,
+                },
+                accessory: {
+                  type: 'button',
+                  text: {
+                    type: 'plain_text',
+                    text: '사이트 바로가기',
+                  },
+                  url: site,
+                },
+              },
+            ],
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        );
+      } catch (err) {
+        console.log(err);
+      }
+
+      return {
+        status: 'DOWN',
+        site: site,
+        checkedAt: new Date().toISOString(),
+      };
+    }
   }
 }
