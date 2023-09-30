b7673b33e71e20db9dd1984126a54d5db2301db5
jaychang99 <jaychang99@gmail.com>
2023-09-30 23:17:33 +0900

    feat(Slack): 슬랙 리포팅 이전 조건 확인 후 발송으로 변경 및 슬랙리포팅 로직 상용 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -29,13 +29,14 @@ export class AppService {
 
     // get all sites from db
     const sites = await this.prisma.service.findMany();
-    console.log(sites);
 
-    for await (const site of sites) {
+    for (const site of sites) {
+      console.log('checking site', site.url);
       const startTime = new Date().getTime();
 
       try {
         await axios.get(site.url);
+        console.log('SUCCESS');
 
         const endTime = new Date().getTime();
         const successData = {
@@ -44,24 +45,46 @@ export class AppService {
           serviceUuid: site.uuid,
           responseTime: endTime - startTime,
         };
+
+        const latestLog = await this.prisma.log.findFirst({
+          where: { serviceUuid: site.uuid },
+          orderBy: { date: 'desc' },
+        });
+
         await this.prisma.log.create({
           data: successData,
         });
+
+        if (latestLog.value === 'SUCCESS') continue;
+        await axios.post(
+          this.configService.get('SLACK_WEBHOOK_URL'),
+          generateSlackReportingSuccess(site.url),
+          HEADERS,
+        );
       } catch (error) {
+        console.log('ERROR');
         const errorData = {
           type: 'check',
           value: 'ERROR',
           serviceUuid: site.uuid,
           responseTime: null,
         };
+        // check for the latest log and if the latest log is error, don't send slack message
+        const latestLog = await this.prisma.log.findFirst({
+          where: { serviceUuid: site.uuid },
+          orderBy: { date: 'desc' },
+        });
+
         await this.prisma.log.create({
           data: errorData,
         });
-        // await axios.post(
-        //   this.config.get('SLACK_WEBHOOK_URL'),
-        //   generateSlackReporting(site.url, error.code),
-        //   HEADERS,
-        // );
+
+        if (latestLog.value === 'ERROR') continue;
+        await axios.post(
+          this.configService.get('SLACK_WEBHOOK_URL'),
+          generateSlackReporting(site.url, error.code),
+          HEADERS,
+        );
       }
     }
     return;
--- src/utils/generateSlackReportingSuccess.ts
+++ src/utils/generateSlackReportingSuccess.ts
@@ -1,12 +1,12 @@
 export const generateSlackReportingSuccess = (site: string) => {
   return {
-    text: `✅ ${site} 서비스 정상 작동 중입니다. ✅`,
+    text: `✅ ${site} 서비스 오류 상태에서 정상으로 복구 되었습니다. ✅`,
     blocks: [
       {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: `✅ ${site} 서비스 정상 작동 중입니다. ✅`,
+          text: `✅ ${site} 서비스 오류 상태에서 정상으로 복구 되었습니다. ✅`,
         },
         accessory: {
           type: 'button',
